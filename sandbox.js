9d2f31e0d196ffd4903fe7f604aad937e413eb53
Eleanor Kelman <ekelman3@gmail.com>
2024-04-01 08:42:44 -0400

    eliminated solution from lecture

---
+++ sandbox.js
@@ -0,0 +1,50 @@
+/* ----------------------------------------------------- 
+    VOCABULARY TERMS
+----------------------------------------------------- */
+
+// semicolons are optional in JS
+
+/*
+
+    - function: reusable blocks of code
+        - function definition: stores the code to be reused
+        - function call: uses the code
+    - parameter: takes inputted code into a function definition
+    - argument: passes inputted code from a function call
+    - return statement: passes code from inside a function scope to the function call's scope
+    - callback function: function passed as an argument
+
+    - scope: which portion of a file can access a piece of code
+        - global scope: seen by entire file
+        - local scope: seen only within a block of code (often in between curly brackets)
+
+*/
+
+/* ----------------------------------------------------- 
+    NAMED FUNCTIONS
+----------------------------------------------------- */
+
+
+
+/* ----------------------------------------------------- 
+    ANONYMOUS FUNCTIONS
+----------------------------------------------------- */
+
+
+
+/* ----------------------------------------------------- 
+    ARROW FUNCTIONS
+----------------------------------------------------- */
+
+
+
+/* ----------------------------------------------------- 
+    SCOPE
+----------------------------------------------------- */
+
+
+
+/* ----------------------------------------------------- 
+    ARRAY METHODS WITH CALLBACK FUNCTIONS
+----------------------------------------------------- */
+
