0e725a8b4684669f50086dc03bbafd097a81806d
Julia Wey-Burgoyne <juliawey@gmail.com>
2024-04-01 14:34:54 -0400

    finished lecture notes

--- sandbox.js
+++ sandbox.js
@@ -23,28 +23,72 @@
 /* ----------------------------------------------------- 
     NAMED FUNCTIONS
 ----------------------------------------------------- */
+function sayHello() {
+    console.log("Hello there!")
+}
 
+//sayHello()
+
+function sayHelloToYou(name) {
+    console.log(`Hello there, ${name}!`)
+}
+//sayHelloToYou("Nick")
+
+function addTwoNumbers(num1, num2) {
+   // console.log(num1 + num2)
+    return num1 + num2
+}
+addTwoNumbers(2, 4)
+
+//sum = addTwoNumbers(5, 400)
+
+//console.log(sum)
 
 
 /* ----------------------------------------------------- 
     ANONYMOUS FUNCTIONS
 ----------------------------------------------------- */
+const squaredNum = function (num) {
+    return num ** 2
+}
 
+square = squaredNum(6)
 
+//console.log(square)
+ 
 
 /* ----------------------------------------------------- 
     ARROW FUNCTIONS
 ----------------------------------------------------- */
+
+const cubedNum = (num) => { return num ** 3}
+
+cube = cubedNum(3)
+
+//console.log(cube)
 
 
 
 /* ----------------------------------------------------- 
     SCOPE
 ----------------------------------------------------- */
+let num1 = 3
 
+function incrementer() {
+    let num2 = 8
 
+    num1 = num1 + 1
+    num2 = num2 + 1
+
+    return num2
+}
+
+console.log(`num1: ${num1}`)
 
 /* ----------------------------------------------------- 
     ARRAY METHODS WITH CALLBACK FUNCTIONS
 ----------------------------------------------------- */
 
+const num = [1, 2, 3, 4, 5, 6]
+
+//num.forEach((num) => console.log(num+1))
